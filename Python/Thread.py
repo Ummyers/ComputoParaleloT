023812af64416fef8b2f6d511b3d71c62c06b300
ummyers <mu.andrea@ciencias.unam.mx>
2021-03-16 13:13:45 -0600

    Viz Tracer

---
+++ Python/Thread.py
@@ -0,0 +1,25 @@
+from viztracer import VizTracer
+import threading
+import time
+
+with VizTracer(output_file="graph.html") as tracer: 
+
+	def worker():
+	    print(threading.current_thread().getName(), 'Starting')
+	    time.sleep(0.2)
+	    print(threading.current_thread().getName(), 'Exiting')
+
+
+	def my_service():
+	    print(threading.current_thread().getName(), 'Starting')
+	    time.sleep(0.3)
+	    print(threading.current_thread().getName(), 'Exiting')
+
+
+	t = threading.Thread(name='my_service', target=my_service)
+	w = threading.Thread(name='worker', target=worker)
+	w2 = threading.Thread(target=worker)  # use default name
+
+	w.start()
+	w2.start()
+	t.start()
